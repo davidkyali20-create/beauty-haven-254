0f110bb6d44803a98037116ab8a7952491c2627b
David Charlie <davidkyali20@gmail.com>
2026-05-13 17:43:57 +0300

    feat: Update service prices and names
    Adjusted pricing and service names within the constants file to reflect current offerings at Beauty Haven 254. This ensures accurate display of services and their associated costs.

--- src/constants.ts
+++ src/constants.ts
@@ -9,37 +9,35 @@ export const SERVICES_DATA: Service[] = [
   { name: "Manicure", price: "500", category: "Manicure" },
   { name: "Gel", price: "500", category: "Manicure" },
   { name: "Manicure/Gel", price: "800", category: "Manicure" },
-  { name: "Tips gel", price: "1000", category: "Manicure" },
-  { name: "Builder gel", price: "1000", category: "Manicure" },
-  { name: "Gum gel", price: "1500", category: "Manicure" },
+  { name: "Tips gel", price: "1500", category: "Manicure" },
+  { name: "Builder gel", price: "1500", category: "Manicure" },
+  { name: "Gum gel", price: "1000", category: "Manicure" },
   { name: "Acrylic", price: "2500", category: "Manicure" },
 
   // Pedicure
   { name: "Pedicure plain", price: "800", category: "Pedicure" },
-  { name: "Gel", price: "500", category: "Pedicure" },
   { name: "Pedicure Gel", price: "1300", category: "Pedicure" },
   { name: "Jelly pedicure", price: "1500", category: "Pedicure" },
 
   // Hair
-  { name: "Knotless", price: "1500", category: "Hair" },
+  { name: "Knotless", price: "2000", category: "Hair" },
   { name: "Plain lines", price: "500", category: "Hair" },
   { name: "Wash and blow", price: "500", category: "Hair" },
-  { name: "Wash and straightening", price: "300", category: "Hair" },
-  { name: "Concrows", price: "1000", category: "Hair" },
+  { name: "Cornrows", price: "2000", category: "Hair" },
   { name: "Wig installation", price: "1500", category: "Hair" },
   { name: "Flat iron", price: "500", category: "Hair" },
-  { name: "Treatments", price: "500", category: "Hair" },
-  { name: "Deep treatment", price: "800", category: "Hair" },
+  { name: "Treatment leave-in", price: "1500", category: "Hair" },
+  { name: "Deep treatment", price: "1000", category: "Hair" },
   { name: "Dreadlocks", price: "3000", category: "Hair" },
-  { name: "Re-touch", price: "1000", category: "Hair" },
-  { name: "Re-touch(chem)", price: "1200", category: "Hair" },
+  { name: "Retouch", price: "1500", category: "Hair" },
+  { name: "Retouch (chem)", price: "1500", category: "Hair" },
   { name: "Scrubbing", price: "500", category: "Hair" },
+  { name: "Coco twist", price: "2000", category: "Hair" },
 
   // Glam
-  { name: "Make up", price: "1500", category: "Glam" },
-  { name: "Lashes", price: "From 500", category: "Glam" },
+  { name: "Make up", price: "1000", category: "Glam" },
 
-  // Other (Important Note)
+  // Other
   { name: "Soak off", price: "200", category: "Other" },
   { name: "Cut file", price: "300", category: "Other" },
 ];
